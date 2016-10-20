a31df5b4a7601156210d3f6e4189504a140abcab
Juan Ojeda <juan@juanojeda.com>
2016-10-21 01:39:51 +1100

    some extras

--- bots.js
+++ bots.js
@@ -28,15 +28,15 @@ const LANG               = 'en-GB';
 const ANSWER_COLORS      = ['#6abf2d','#bfe560','#e9eeed']
 
 /**
- * Gets an array of all the questions from Contentful, in a useful format
- * @param  {object} client the contentful connection class
- * @return {array}         an array of question objects
- */
-const fetchQuestions = (client) => {
+* Gets an array of all the questions from Contentful, in a useful format
+* @param  {object} CMS the contentful connection class
+* @return {array}         an array of question objects
+*/
+const fetchQuestions = (CMS) => {
   let questions = [];
   let isAllEntriesAdded = false;
 
-  client.getSpace(SPACE_ID)
+  CMS.getSpace(SPACE_ID)
   .then( (space) => {
     space.getEntries()
     .then( (entries) => {
@@ -58,42 +58,88 @@ const fetchQuestions = (client) => {
 }
 
 /**
- * response routine that runs the following algorithm:
- *    - Tries to find a direct match for user's question
- *    - If there's a direct match, returns the corresponding answer, and asks
- *      newbie if this answers their question
- *    - If no direct match, turns question into keywords, and returns all
- *      questions with a matching keyword (sorted by highest no. of matches).
- *    - If no matches, logs question as an unanswered question
- * @param  {[type]} bot      [description]
- * @param  {[type]} question [description]
- * @return {[type]}          [description]
- */
-function respondToQuestion(bot, question){
+* response routine that runs the following algorithm:
+*    - Tries to find a direct match for user's question
+*    - If there's a direct match, returns the corresponding answer, and asks
+*      newbie if this answers their question
+*    - If no direct match, turns question into keywords, and returns all
+*      questions with a matching keyword (sorted by highest no. of matches).
+*    - If no matches, logs question as an unanswered question
+* @param  {[type]} bot      [description]
+* @param  {[type]} question [description]
+* @return {[type]}          [description]
+*/
+function respondToQuestion(bot, message, questionsStore){
 
+  let directMatch = getDirectMatch(message,questionsStore);
+  let response;
+  let responseMethod = 'short';
+
+  if (directMatch.answerStatus){
+    response = directMatch.answer;
+  } else {
+    let keywords = getKeywords(message.text);
+    let keywordMatches = getKeywordMatches(keywords, questionsStore);
+
+    if (keywordMatches.answerStatus){
+      let attachments = [];
+      keywordMatches.answers.map((answer, index) => {
+        let colorIndex = index > 2 ? 2 : index;
+        let answerHash = {
+          "title": answer.question,
+          "text": answer.response,
+          "color": ANSWER_COLORS[colorIndex]
+        };
+        attachments.push(answerHash);
+      });
+
+      if (attachments.length > 1){
+        responseMethod = 'long';
+      }
+
+      response = {
+        "text": `Here's what I found...`,
+        "attachments": attachments
+      }
+    }
+    else {
+      response = {
+        "text": `I err... what's that again?`
+      }
+    }
+  }
+
+  let makeResponse = (message, response) => {
+    return () => {
+      bot.reply(message, response);
+    }
+  };
+
+  bot.startTyping(message);
+  setTimeout(makeResponse(message, response), RESPONSE_METHOD[responseMethod]);
 }
 
 /**
- * Utility to parse sentence into a usable format
- * @param  {string} sentence    a sentence that needs parsing
- * @return {string} parsed      the parsed sentence
- */
+* Utility to parse sentence into a usable format
+* @param  {string} sentence    a sentence that needs parsing
+* @return {string} parsed      the parsed sentence
+*/
 function parseSentence(sentence){
   const PUNCTUATION   = /[.,\/#!$%\^&\*;:{}=\-_`~()?]/g;
   const MULTISPACE    = /[\s]{2,}/g;
   const depunctuated  = sentence.replace(PUNCTUATION, " ")
-                        .replace(MULTISPACE, " ").trim();
+  .replace(MULTISPACE, " ").trim();
   const parsed        = depunctuated.toLowerCase();
 
   return parsed;
 }
 
 /**
- * Takes the newbie's question, strips out useless words (eg. it, and, of, etc),
- * and returns an array of keywords
- * @param  {string} question  The newbie's question
- * @return {array} keywords   The useful keywords
- */
+* Takes the newbie's question, strips out useless words (eg. it, and, of, etc),
+* and returns an array of keywords
+* @param  {string} question  The newbie's question
+* @return {array} keywords   The useful keywords
+*/
 function getKeywords(question){
   let keywords = [];
   let words = parseSentence(question).split(' ');
@@ -109,14 +155,14 @@ function getKeywords(question){
 }
 
 /**
- * takes the newbie's question and attempts to match directly to a listed
- * question. If a direct match is found, return the corresponding answer.
- * Otherwise, return false.
- * @param  {string} question        The newbie's question
- * @param  {array} questionsStore   The fetched questions from the CMS
- * @return {object} response        Returns a boolean answerStatus, and an
- *                                  optional answer message.
- */
+* takes the newbie's question and attempts to match directly to a listed
+* question. If a direct match is found, return the corresponding answer.
+* Otherwise, return false.
+* @param  {string} question        The newbie's question
+* @param  {array} questionsStore   The fetched questions from the CMS
+* @return {object} response        Returns a boolean answerStatus, and an
+*                                  optional answer message.
+*/
 function getDirectMatch(question, questionsStore){
   const response = {
     answerStatus: false,
@@ -138,14 +184,15 @@ function getDirectMatch(question, questionsStore){
 }
 
 /**
- * Takes an array of keywords, and iterates over the questions store
- * to find a question that has matching keywords
- * @param  {array} keywords         The asked question's keywords
- * @param  {object} questionsStore  The fetched questions from the CMS
- * @return {object} keywordMatches  A response object with a status and an
- *                                  optional array of matching response objects
- */
+* Takes an array of keywords, and iterates over the questions store
+* to find a question that has matching keywords
+* @param  {array} keywords         The asked question's keywords
+* @param  {object} questionsStore  The fetched questions from the CMS
+* @return {object} keywordMatches  A response object with a status and an
+*                                  optional array of matching response objects
+*/
 function getKeywordMatches(keywords, questionsStore){
+
   const keywordMatches = {
     answerStatus: false,
     answers: []
@@ -186,67 +233,65 @@ function init(){
     debug: false,
   });
 
+  const CMS = contentful.createClient({
+    accessToken: process.env.CONTENTFUL_KEY
+  });
+
+  const questionsStore = fetchQuestions(CMS);
+
   const nooBot = controller.spawn({
     token: process.env.SLACKBOT_TOKEN
   }).startRTM();
 
-
-  const client = contentful.createClient({
-    accessToken: process.env.CONTENTFUL_KEY
-  });
-
-  const questionsStore = fetchQuestions(client);
-
-
   controller.on([AT_MENTION, DM], (bot, message) => {
 
-    let directMatch = getDirectMatch(message,questionsStore);
-    let response;
-    let responseMethod = 'short';
-
-    if (directMatch.answerStatus){
-      response = directMatch.answer;
-    } else {
-      let keywords = getKeywords(message.text);
-      let keywordMatches = getKeywordMatches(keywords, questionsStore);
-
-      if (keywordMatches.answerStatus){
-        let attachments = [];
-        keywordMatches.answers.map((answer, index) => {
-          let colorIndex = index > 2 ? 2 : index;
-          let answerHash = {
-            "title": answer.question,
-            "text": answer.response,
-            "color": ANSWER_COLORS[colorIndex]
-          };
-          attachments.push(answerHash);
-        });
-
-        if (attachments.length > 1){
-          responseMethod = 'long';
-        }
-
-        response = {
-          "text": `Here's what I found...`,
-          "attachments": attachments
-        }
-      } else {
-        // TODO: turn this into a fallback function
-        response = `Well this is embarassing, but I don't know the answer to that! Try rephrasing your question, or asking ${FALLBACK_CONTACT}`;
-      }
-
-    }
-
-    bot.startTyping(message);
-    setTimeout(() => {
-      bot.reply(message, response);
-    }, RESPONSE_METHOD[responseMethod]);
+    respondToQuestion(bot, message, questionsStore);
 
   });
 
   controller.on([SUB_MENTION], (bot, message) => {
-    console.log(message);
-    bot.replyPrivate(message, `hey <@${message.user}>, did you have a question I can help you with?`);
+
+    bot.startPrivateConversation(
+      {
+        user: message.user
+      }, (err, convo) => {
+      if (err){
+        // do the error logging
+      } else {
+        convo.ask(`hey <@${message.user}>, did you have a question I can help you with?`, [
+          {
+            pattern: bot.utterances.yes,
+            callback: (response, convo) => {
+              convo.say(`Alright, hit me!`);
+              convo.next();
+            }
+          },
+          {
+            pattern: bot.utterances.no,
+            callback: (response, convo) => {
+              convo.say(`No probs!`);
+              convo.next();
+            }
+          },
+          {
+            default: true,
+            callback: (response, convo) => {
+              convo.next();
+              let newMsg = message;
+
+              bot.api.im.list({}, (error, data) => {
+                let userIm = data.ims.map((im) => {
+                  return im.user === message.user;
+                });
+                newMsg.channel = userIm.id;
+                respondToQuestion(bot, newMsg, questionsStore);
+              });
+
+            }
+          }
+        ]);
+      }
+    });
   });
 
 }
